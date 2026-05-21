4d31356da709eb4541f9833ceeee78d8ec1f64fa
Daniel Lewandowski <daniellewandowski527@gmail.com>
2026-05-21 04:37:38 +0200

    fix: correct PythonAnywhere project path to AntigravityProjekt

--- wsgi_pythonanywhere.py
+++ wsgi_pythonanywhere.py
@@ -2,11 +2,11 @@ import sys
 import os
 
 # Ścieżka do Twojego projektu na PythonAnywhere
-path = '/home/BLADERUNNER009/POLSKIBOT'
+path = '/home/BLADERUNNER009/AntigravityProjekt'
 if path not in sys.path:
     sys.path.insert(0, path)
 
-# Ustawiamy zmienne środowiskowe z pliku .env ręcznie (PythonAnywhere nie ładuje .env automatycznie)
+# Ustawiamy zmienne środowiskowe z pliku .env (PythonAnywhere nie ładuje .env automatycznie)
 from dotenv import load_dotenv
 load_dotenv(os.path.join(path, '.env'))
 
